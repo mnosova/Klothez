268c6c43afc3b34cf028772a21c1ed3174813bf9
Marina Nosova <nosova_marino4ka@mail.ru>
2019-04-12 19:26:19 +0300

    project init

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const config = {
+    mode: 'development',
+    entry: {
+        form: './src/js/src/main.js'
+    },
+    output: {
+        filename: "[name].js"
+    },
+    plugins: [
+        new UglifyJSPlugin({
+            // sourceMap: true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        ]
+    },
+    externals: {
+        jquery: 'jQuery'
+    }
+
+};
+module.exports = config;
