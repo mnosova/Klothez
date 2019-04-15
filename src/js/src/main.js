5ae94f7edb061d1d96774df961c21e88d2b78d6b
Marina Nosova <nosova_marino4ka@mail.ru>
2019-04-15 12:04:53 +0300

    header menu animate

--- src/js/src/main.js
+++ src/js/src/main.js
@@ -1,8 +1,11 @@
+import headerMenu from './modules/header';
 import deliveryFaq from './modules/delivery-faq';
 import formReset from './modules/form-reset';
 
 $( document ).ready(function() {
+    headerMenu();
     deliveryFaq();
     formReset();
+
 
 });
---
+++ src/js/src/modules/header.js
@@ -0,0 +1,13 @@
+const headerMenu = function () {
+    let $bigItem = $('.header-secondmenu__navigation');
+    let $smallItem = $('.header-menu__navigation_category');
+    $smallItem.on('mouseenter', function () {
+        $bigItem.hide();
+        $smallItem.removeClass('js-show');
+        $bigItem.eq($(this).index()).show();
+        $smallItem.eq($(this).index()).addClass('js-show');
+    });
+
+    $bigItem.on('mouseleave',  () => $bigItem.hide());
+};
+export default headerMenu;
